a27cc27ab56aa0b3682f1b59245e727e4109afeb
Paul de Jong <riderpaul@gmail.com>
2021-02-16 00:09:42 -0800

    Simple OO demo

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,14 @@
 import '../styles/styles.css';
+import Person from './modules/Person';
 
 if (module.hot) {
     module.hot.accept()
 }
+
+/* Lesson example code */
+
+let john = new Person ("John Doe", "red");
+john.greet();
+
+let jane = new Person ("Jane Smith", "green");
+jane.greet();
