0f6a6c5a1451690de85a13088443d0f8d2bb467f
Paul de Jong <riderpaul@gmail.com>
2021-02-18 20:59:37 -0800

    added icon affect for small screens

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,14 +1,9 @@
 import '../styles/styles.css';
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu;
 
 if (module.hot) {
     module.hot.accept()
 }
 
-/* Lesson example code */
-
-let john = new Person ("John Doe", "red");
-john.greet();
-
-let jane = new Person ("Jane Smith", "green");
-jane.greet();
