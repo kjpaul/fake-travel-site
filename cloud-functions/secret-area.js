0feb1a3c9bb44fc2d744e7cc6905667420e0a734
Paul de Jong <riderpaul@gmail.com>
2021-02-19 16:44:43 -0800

    Updated cloud function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,26 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the super secret area"
-    })
+    const secretContent = `
+    <h3>Welcome to the super secret area</h3>
+    Yada, Yada, <strong>Yada!</strong>
+    `
+    
+    let body
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password === "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+
+    }
 }
